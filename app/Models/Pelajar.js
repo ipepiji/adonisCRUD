3cbc300b75de4411b984d64d0fa6c9f992fd4784
hafizhi <hafizhi.z@okwave.global>
2019-09-23 10:22:00 +0800

    info

--- app/Models/Pelajar.js
+++ app/Models/Pelajar.js
@@ -5,7 +5,7 @@ const Model = use('Model')
 
 class Pelajar extends Model {
     static get table () {
-        return 'students' //link ke db table mana
+        return 'students' //link ke db table mana, kalau xde ni..die cri table pelajars kt db as default
       }
 }
 
